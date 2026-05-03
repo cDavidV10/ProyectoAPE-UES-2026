4adb18ee58160c06ee6c2bd216f408c745412c7e
David <cdavidvasquezr20@gmail.com>
2026-05-03 11:59:05 -0600

    Evitando que el usuario pueda modificar las dimensiones de la ventana

--- src/main/java/vista/Login.java
+++ src/main/java/vista/Login.java
@@ -18,6 +18,7 @@ public class Login extends javax.swing.JFrame {
      * Creates new form Login
      */
     public Login() {
+        this.setResizable(false);
         initComponents();
         jLabel1.requestFocusInWindow();
     }
