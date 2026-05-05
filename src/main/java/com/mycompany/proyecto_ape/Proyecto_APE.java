d1bfffa07b4b350c6f8ba01513efe30684f13625
David <cdavidvasquezr20@gmail.com>
2026-05-04 23:34:29 -0600

    Eliminando imports innecesarios

--- src/main/java/com/mycompany/proyecto_ape/Proyecto_APE.java
+++ src/main/java/com/mycompany/proyecto_ape/Proyecto_APE.java
@@ -1,9 +1,8 @@
-package com.mycompany.proyecto_ape;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
 
-import controlador.ControladorAdministrarCursos;
-import controlador.ControladorRegistrarCursos;
-import vista.AdministrarCursos;
-import vista.RegistrarCursos;
+package com.mycompany.proyecto_ape;
 
 import controlador.CtrlLogin;
 import vista.Login;
@@ -20,8 +19,3 @@ public class Proyecto_APE {
         login.setVisible(true);
     }
 }
-}
-/*          VistaRegEstu vista = new VistaRegEstu();
-            new ControllerRegEstu(vista);
-            vista.setLocationRelativeTo(null);
-            vista.setVisible(true);*/
