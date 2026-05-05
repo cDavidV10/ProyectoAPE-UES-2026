fcf45248ed556a33a415e72817cb2d56a90f03d6
acevedoDaniel <danielacevedoc0@gmail.com>
2026-05-05 09:11:37 -0600

    agregando insertar las credenciales a la bd

--- src/main/java/dao/ConsultaRegistro.java
+++ src/main/java/dao/ConsultaRegistro.java
@@ -5,6 +5,7 @@
 package dao;
 
 import conexion.Conexion;
+import interfaz.ICredencialesDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,9 +15,12 @@ import java.sql.SQLException;
  *
  * @author MINEDUCYT
  */
-public class ConsultaRegistro {
+public class ConsultaRegistro implements ICredencialesDAO {
     private String SELECT = "";
-    public String buscarRegistro(String tipo, String nombre, String apellido) throws SQLException{
+    private String INSERT = "";
+    
+    @Override
+    public final String buscarRegistro(String tipo, String nombre, String apellido) throws SQLException{
         String resultado = "No hay resultado";
         String iniciales = "" + nombre.charAt(0) + apellido.charAt(0);
         SELECT = "SELECT user_name FROM usuario WHERE user_name Like ? ORDER BY user_name DESC LIMIT 1";
@@ -36,7 +40,8 @@ public class ConsultaRegistro {
         return resultado;
     }
     
-    public int buscarRegistroUserID()throws SQLException{
+    @Override
+    public final int buscarRegistroUserID()throws SQLException{
         int resultado = -1;
         SELECT = "SELECT id_usuario FROM usuario ORDER BY id_usuario DESC LIMIT 1";
         
@@ -52,5 +57,24 @@ public class ConsultaRegistro {
         ps.close();
         conexion.close();
         return resultado;
+    }
+
+    @Override
+    public final void registrarCredenciales(String user, String contra, String tipo) throws SQLException {
+        INSERT = "INSERT INTO usuario (user_name, password, tipo) VALUES (?,?,?)";
+        
+        try (Connection conexion = Conexion.getConexion()) {
+            conexion.setAutoCommit(false);
+            PreparedStatement ps = conexion.prepareStatement(INSERT);
+            ps.setString(1, user);
+            ps.setString(2, contra);
+            ps.setString(3, tipo);
+            
+            ps.executeUpdate();
+            conexion.commit();
+            
+        } catch (Exception ex) {
+            throw ex;
+        }
     }
 }
