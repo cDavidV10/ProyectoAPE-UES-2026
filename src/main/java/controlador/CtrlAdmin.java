6360ee60e1cc20d7b4d6e47557fd90ca08c9210c
David <cdavidvasquezr20@gmail.com>
2026-05-23 10:51:20 -0600

    Renombrando clase a Estudiante

--- src/main/java/controlador/CtrlAdmin.java
+++ src/main/java/controlador/CtrlAdmin.java
@@ -17,7 +17,7 @@ import funciones.Paneles;
 import funciones.UsuarioActivo;
 
 import java.awt.event.WindowEvent;
-import modelo.ModelRegEstu;
+import modelo.Estudiante;
 import modelo.Usuario;
 import vista.AdminView;
 import vista.AdministrarCursos;
@@ -86,8 +86,8 @@ public class CtrlAdmin {
         modelo.setRowCount(0);
 
         try {
-            List<ModelRegEstu> lista = dao.listar();
-            for (ModelRegEstu e : lista) {
+            List<Estudiante> lista = dao.listar();
+            for (Estudiante e : lista) {
                 modelo.addRow(new Object[] {
                         e.getIdEstudiante(),
                         e.getNombre(),
