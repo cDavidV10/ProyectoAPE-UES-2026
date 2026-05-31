91193947df75d8bca91342267ec68c40986b4d65
David <cdavidvasquezr20@gmail.com>
2026-05-30 18:54:16 -0600

    Agregando bgContent y mejorando la estructura de EstudianteView; inicializando Paneles en CtrlEstudianteView

--- src/main/java/controlador/CtrlEstudianteView.java
+++ src/main/java/controlador/CtrlEstudianteView.java
@@ -1,5 +1,6 @@
 package controlador;
 
+import funciones.Paneles;
 import funciones.UsuarioActivo;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -15,11 +16,13 @@ public class CtrlEstudianteView {
     private EstudianteView estudianteView;
     private Usuario usuario;
     private Login login;
+    private Paneles paneles;
 
     public CtrlEstudianteView(EstudianteView estudianteView, Usuario usuario, Login login) {
         this.estudianteView = estudianteView;
         this.usuario = usuario;
         this.login = login;
+        this.paneles = new Paneles();
 
         estudianteView.addWindowListener(new WindowAdapter() {
             @Override
@@ -46,7 +49,8 @@ public class CtrlEstudianteView {
         CursosDisponiblesView cursosView = new CursosDisponiblesView();
         CursosDisponiblesDAO cursosDAO = new CursosDisponiblesDAO();
         new CtrlCursosDisponibles(cursosDAO, cursosView, idEstudiante);
-        cursosView.setVisible(true);
+        paneles.insertarPaneles(cursosView, this.estudianteView.getBgContent());
+
     }
 
 }
