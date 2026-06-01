de8ea2a05c020d336fdfc7baf3ce34d2e7e7b4e5
David <cdavidvasquezr20@gmail.com>
2026-05-31 18:58:48 -0600

    Agregando funcion para actualizar

--- src/main/java/dao/PeriodoInscripcionDAO.java
+++ src/main/java/dao/PeriodoInscripcionDAO.java
@@ -17,6 +17,63 @@ import modelo.PeriodoInscripcion;
 public class PeriodoInscripcionDAO implements IInscripcionDAO {
 
     @Override
+    public void insertar(String username, PeriodoInscripcion periodoInscripcion) throws Exception {
+        String consulta = """
+                insert into periodo_inscripcion(id_administrador, id_inicio_curso, fecha_apertura, fecha_cierre)
+                SELECT id_admind, null, ?, ?
+                from usuario us
+                where us.username = ?;
+                                """;
+
+        Connection conexion = Conexion.getConexion();
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+
+            ps.setObject(1, periodoInscripcion.getFechaApertura());
+            ps.setObject(2, periodoInscripcion.getFechaCierre());
+            ps.setString(3, username);
+            conexion.setAutoCommit(false);
+
+            ps.executeUpdate();
+            conexion.commit();
+        } catch (Exception e) {
+            conexion.rollback();
+            throw e;
+        } finally {
+            conexion.close();
+        }
+
+    }
+
+    @Override
+    public void actualizar(PeriodoInscripcion periodoInscripcion) throws Exception {
+        String consulta = """
+                update periodo_inscripcion
+                set fecha_cierre = ?
+                where id_periodo = ?;
+                                    """;
+
+        Connection conexion = Conexion.getConexion();
+
+        try {
+            PreparedStatement ps = conexion.prepareStatement(consulta);
+
+            ps.setObject(1, periodoInscripcion.getFechaCierre());
+            ps.setInt(2, periodoInscripcion.getId());
+            conexion.setAutoCommit(false);
+
+            ps.executeUpdate();
+            conexion.commit();
+        } catch (Exception e) {
+            conexion.rollback();
+            throw e;
+        } finally {
+            conexion.close();
+        }
+    }
+
+    @Override
     public List<InicioCurso> listar() throws Exception {
         String cursosDisponibles = """
                 SELECT
@@ -68,6 +125,51 @@ public class PeriodoInscripcionDAO implements IInscripcionDAO {
         conexion.close();
         return datos;
 
+    }
+
+    @Override
+    public boolean existePeriodoActivo() throws Exception {
+        String consulta = """
+                    select count(*) from periodo_inscripcion
+                    where estado = 'Activo';
+                """;
+
+        Connection conexion = Conexion.getConexion();
+        PreparedStatement ps = conexion.prepareStatement(consulta);
+
+        ResultSet rs = ps.executeQuery();
+
+        rs.next();
+
+        conexion.close();
+        return rs.getInt(1) > 0;
+    }
+
+    @Override
+    public PeriodoInscripcion periodoActivo() throws Exception {
+        String consulta = """
+                select
+                    id_periodo,
+                    fecha_apertura,
+                    fecha_cierre
+                from periodo_inscripcion
+                where estado = 'Activo';
+                                """;
+
+        Connection conexion = Conexion.getConexion();
+        PreparedStatement ps = conexion.prepareStatement(consulta);
+
+        ResultSet rs = ps.executeQuery();
+
+        PeriodoInscripcion periodoInscripcion = new PeriodoInscripcion();
+
+        if (rs.next()) {
+            periodoInscripcion.setId(rs.getInt("id_periodo"));
+            periodoInscripcion.setFechaApertura(rs.getObject("fecha_apertura", LocalDate.class));
+            periodoInscripcion.setFechaCierre(rs.getObject("fecha_cierre", LocalDate.class));
+        }
+
+        return periodoInscripcion;
     }
 
 }
--- src/main/java/interfaz/IInscripcionDAO.java
+++ src/main/java/interfaz/IInscripcionDAO.java
@@ -3,7 +3,16 @@ package interfaz;
 import java.util.List;
 
 import modelo.InicioCurso;
+import modelo.PeriodoInscripcion;
 
 public interface IInscripcionDAO {
+    public void insertar(String username, PeriodoInscripcion periodoInscripcion) throws Exception;
+
+    public void actualizar(PeriodoInscripcion periodoInscripcion) throws Exception;
+
     public List<InicioCurso> listar() throws Exception;
+
+    public boolean existePeriodoActivo() throws Exception;
+
+    public PeriodoInscripcion periodoActivo() throws Exception;
 }
