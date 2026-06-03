459b2ef5d2a2bb00c05adc6fee997f522393cdb7
David <cdavidvasquezr20@gmail.com>
2026-06-03 04:08:11 -0600

    Agregar funcion para actualizar informacion

--- src/main/java/controlador/CtrlAdminInscripcion.java
+++ src/main/java/controlador/CtrlAdminInscripcion.java
@@ -2,6 +2,7 @@ package controlador;
 
 import java.util.List;
 
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 import dao.PeriodoInscripcionDAO;
@@ -55,8 +56,16 @@ public class CtrlAdminInscripcion {
 
         inscripcionView.getJtCursos().getSelectionModel().addListSelectionListener(e -> {
             boolean seleccionado = inscripcionView.getJtCursos().getSelectedRow() != -1;
-            inscripcionView.getBtnAgregar().setEnabled(true);
 
+            if (seleccionado) {
+
+                inscripcionView.getBtnAgregar().setEnabled(true);
+            }
+
+        });
+
+        inscripcionView.getBtnAgregar().addActionListener(e -> {
+            agregarCursoPeriodo();
         });
 
     }
@@ -106,6 +115,23 @@ public class CtrlAdminInscripcion {
 
             this.inscripcionView.getJtCursos().setModel(modelo);
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void agregarCursoPeriodo() {
+        int fila = inscripcionView.getJtCursos().getSelectedRow();
+        String codigo = (String) inscripcionView.getJtCursos().getValueAt(fila, 0);
+
+        try {
+            periodoDAO.agregarCurso(codigo);
+
+            JOptionPane.showMessageDialog(null, "Curso agregado correctamente", "Operacion Exitosa",
+                    JOptionPane.INFORMATION_MESSAGE);
+            mostrarDatos();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al agregar curso", "Operacion Fallida",
+                    JOptionPane.ERROR_MESSAGE);
             System.out.println(e.getMessage());
         }
     }
